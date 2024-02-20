cd04116e40b9e27c9bfac9af6f2589a8b418d2b1
slvrtrn <hypnoash@gmail.com>
2024-02-20 01:26:29 +0100

    AES Reader

---
+++ destination/encryption/aes/aes_test.go
@@ -0,0 +1,94 @@
+package aes
+
+import (
+	"encoding/base64"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var key, _ = base64.StdEncoding.DecodeString("VyEZCkPngvf4mtRHemjGkC6tmd/22j0R9z+DQv2he/Q=")
+
+var fileName = "../../../tests/resources/campaign.csv.zst.aes"
+var expectedFileName = "../../../tests/resources/campaign.csv.zst"
+
+func TestAES256ReaderReadAll(t *testing.T) {
+	file, err := os.Open(fileName)
+	assert.NoError(t, err)
+	defer file.Close()
+	expected, err := os.ReadFile(expectedFileName)
+	assert.NoError(t, err)
+
+	reader, err := NewReader(file, key)
+	assert.NoError(t, err)
+	assert.IsType(t, &Decoder{}, reader)
+
+	result, err := io.ReadAll(reader)
+	assert.NoError(t, err)
+
+	assert.Equal(t, len(expected), len(result))
+	assert.Equal(t, expected, result)
+
+	assert.False(t, reader.isClosed)
+	reader.Close()
+	assert.True(t, reader.isClosed)
+
+	n, err := reader.Read(make([]byte, 1))
+	assert.Equal(t, 0, n)
+	assert.Equal(t, err, io.ErrClosedPipe)
+}
+
+func TestAES256ReaderVariousBufferSizes(t *testing.T) {
+	expected, err := os.ReadFile(expectedFileName)
+	assert.NoError(t, err)
+
+	bufferSizes := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 50, 100, 1000}
+	for _, bufferSize := range bufferSizes {
+		file, err := os.Open(fileName)
+		assert.NoError(t, err)
+		reader, err := NewReader(file, key)
+		assert.NoError(t, err)
+		result := readWithBuffer(t, reader, make([]byte, bufferSize))
+		assert.Equal(t, len(expected), len(result), "Buffer size: %d", bufferSize)
+		assert.Equal(t, expected, result, "Buffer size: %d", bufferSize)
+		reader.Close()
+		file.Close()
+	}
+}
+
+func TestAES256ReaderInvalidOpenFile(t *testing.T) {
+	_, err := NewReader(nil, key)
+	assert.ErrorContains(t, err, "file cannot be nil")
+
+	file, err := os.Open(fileName)
+	assert.NoError(t, err)
+	file.Close()
+
+	_, err = NewReader(file, key)
+	assert.ErrorContains(t, err, "use of closed file")
+
+	file2, err := os.Open(fileName)
+	assert.NoError(t, err)
+	defer file2.Close()
+	_, err = io.ReadAll(file2)
+	assert.NoError(t, err)
+	_, err = NewReader(file2, key)
+	assert.Equal(t, err, io.EOF)
+}
+
+func readWithBuffer(t *testing.T, reader io.Reader, buf []byte) (res []byte) {
+	for {
+		n, err := reader.Read(buf)
+		res = append(res, buf[:n]...)
+		if err == io.EOF {
+			break
+		}
+		assert.NoError(t, err)
+		if err != nil {
+			break
+		}
+	}
+	return res
+}
--- destination/service/csv_files.go
+++ destination/service/csv_files.go
@@ -1,14 +1,12 @@
 package service
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
 
+	"fivetran.com/fivetran_sdk/destination/encryption/aes"
 	pb "fivetran.com/fivetran_sdk/proto"
 	"github.com/klauspost/compress/gzip"
 	"github.com/klauspost/compress/zstd"
@@ -24,89 +22,53 @@ func ReadCSVFile(
 	if !ok {
 		return nil, fmt.Errorf("key for file %s not found", fileName)
 	}
-	file, err := os.ReadFile(fileName)
+	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, fmt.Errorf("file %s does not exist", fileName)
 	}
-	decrypted, err := Decrypt(key, file, encryption)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decrypt file %s, cause: %w", fileName, err)
+	defer file.Close()
+
+	var decryptedReader io.Reader
+	if encryption == pb.Encryption_AES {
+		aesReader, err := aes.NewReader(file, key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decrypt file %s, cause: %w", fileName, err)
+		}
+		defer aesReader.Close()
+		decryptedReader = aesReader
+	} else {
+		decryptedReader = file
 	}
-	decompressed, err := Decompress(decrypted, compression)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decompress file %s, cause: %w", fileName, err)
+
+	var decompressedReader io.Reader
+	switch compression {
+	case pb.Compression_ZSTD:
+		zstdReader, err := zstd.NewReader(decryptedReader)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decompress file %s with ZSTD, cause: %w", fileName, err)
+		}
+		defer zstdReader.Close()
+		decompressedReader = zstdReader
+	case pb.Compression_GZIP:
+		gzipReader, err := gzip.NewReader(decryptedReader)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decompress file %s with GZIP, cause: %w", fileName, err)
+		}
+		defer gzipReader.Close()
+		decompressedReader = gzipReader
+	case pb.Compression_OFF:
+		decompressedReader = decryptedReader
 	}
-	csvReader := csv.NewReader(bytes.NewReader(decompressed))
+
+	csvReader := csv.NewReader(decompressedReader)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		return nil, fmt.Errorf("file %s is not a valid CSV, cause: %w", fileName, err)
+		return nil, fmt.Errorf("failed to load CSV file %s with encryption %s and compression %s, cause: %w",
+			fileName, encryption.String(), compression.String(), err)
 	}
 	if len(records) < 2 {
 		return nil, fmt.Errorf("expected to have more than 1 line in file %s", fileName)
 	}
+
 	return records[1:], nil // skip the column names
-}
-
-func Decrypt(key []byte, data []byte, encryption pb.Encryption) ([]byte, error) {
-	switch encryption {
-	case pb.Encryption_AES:
-		return DecryptAES256(key, data)
-	default:
-		return data, nil
-	}
-}
-
-func DecryptAES256(key []byte, data []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(data) < block.BlockSize() {
-		return nil, err
-	}
-	iv, data := data[:block.BlockSize()], data[block.BlockSize():]
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(data, data)
-	return PKCS5Padding(data), nil
-}
-
-func PKCS5Padding(data []byte) []byte {
-	padding := data[len(data)-1]
-	return data[:len(data)-int(padding)]
-}
-
-func Decompress(data []byte, compression pb.Compression) ([]byte, error) {
-	var (
-		res []byte
-		err error
-	)
-	switch compression {
-	case pb.Compression_ZSTD:
-		res, err = DecompressZSTD(data)
-	case pb.Compression_GZIP:
-		res, err = DecompressGZIP(data)
-	case pb.Compression_OFF:
-		res, err = data, nil
-	}
-	return res, err
-}
-
-var zstdReader, _ = zstd.NewReader(nil, zstd.WithDecoderConcurrency(0))
-
-func DecompressZSTD(data []byte) ([]byte, error) {
-	return zstdReader.DecodeAll(data, nil)
-}
-
-func DecompressGZIP(data []byte) ([]byte, error) {
-	gzipReader, err := gzip.NewReader(bytes.NewBuffer(data))
-	if err != nil {
-		return nil, err
-	}
-	defer gzipReader.Close()
-	res, err := io.ReadAll(gzipReader)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
 }
